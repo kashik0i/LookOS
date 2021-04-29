b72951d6bd0f9ea05b0bb82ab3fd155484b85252
Amr Abdelrahman Badawy <amr.badawy13@gmail.com>
2021-04-29 14:30:14 +0200

    implement print routine

--- boot.asm
+++ boot.asm
@@ -3,12 +3,25 @@ BITS 16
 
 
 start:
-  mov ah, 0eh
-  mov al, 'A'
-  mov bx, 0
-  int 0x10 
+  mov si, message ; Read address of 'message'
+  call print
   jmp $
 
+print:
+  mov bx,0 ; Page number
+.loop:
+  lodsb ; Load byte into AL from SI register
+  cmp al,0
+  je .done
+  call print_char
+  jmp .loop
+.done:
+  ret
+print_char:
+  mov ah, 0eh ;  Display a character on the screen, advancing the cursor and scrolling the screen as necessary
+  int 0x10 ; Video teletype output AL=char to write BH=page number BL=foreground color (later)
+  ret
 
+message: db 'Hello, World!', 0
 times 510 - ($-$$) db 0
 dw 0xaa55
